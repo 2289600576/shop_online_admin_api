7d25a19f9652678ba3b696eb123a5ad206f0fd39
吴云辉 <wyh2289600576@163.com>
2023-11-28 09:32:12 +0800

    管理员框架

---
+++ src/main/java/com/shop_online/service/GoodsService.java
@@ -0,0 +1,16 @@
+package com.shop_online.service;
+
+import com.shop_online.entity.Goods;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author wyh
+ * @since 2023-11-28
+ */
+public interface GoodsService extends IService<Goods> {
+
+}
---
+++ src/main/java/com/shop_online/service/TbBrandService.java
@@ -0,0 +1,16 @@
+package com.shop_online.service;
+
+import com.shop_online.entity.TbBrand;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author wyh
+ * @since 2023-11-28
+ */
+public interface TbBrandService extends IService<TbBrand> {
+
+}
